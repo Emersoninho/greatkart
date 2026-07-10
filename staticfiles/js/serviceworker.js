b443a0c2778e08f107f3feca9d1d86a94cf462bf
Emerson <emersoneletrotecnico2013@hotmail.com>
2026-07-10 17:09:59 -0300

    Correção caminho imagens

---
+++ staticfiles/js/serviceworker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'greatkart-v1';
+const urlsToCache = [
+    '/',
+    '/store/',
+    '/static/css/custom.css',
+];
+
+// Instala o Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+// Busca do cache (offline first)
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
+
+// Atualiza cache
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
